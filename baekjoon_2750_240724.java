0c774b9b6dd0648e436ec71cca9e15584f500ae3
Youngseo choi <161001676+YoungseoChoi8482@users.noreply.github.com>
2024-07-24 23:05:25 +0900

    버블정렬 기초문제

---
+++ baekjoon_2750_240724.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+public class baekjoon_2750_240724 {
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int [] arr= new int[N];
+
+        for(int i = 0; i < N; i++)
+        {
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i = 0; i < N-1; i++)
+        {
+            for(int j = 0; j < N - 1 - i; j++)
+            if(arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+
+        for(int i = 0; i < N; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}

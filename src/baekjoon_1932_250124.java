13fb643efa6c8be479557f3a53b0360c1e14da9b
YoungseoChoi8482 <cys03380338@gmail.com>
2025-01-25 20:50:11 +0900

    dynamic programming

--- src/baekjoon_1932_250124.java
+++ src/baekjoon_1932_250124.java
@@ -1,35 +1,39 @@
-
 import java.util.*;
 import java.io.*;
 
 public class baekjoon_1932_250124 {
-    public static void main(String[] args) throws IOException{
+
+    static int N;
+    static int[][] arr;
+    static Integer[][] dp;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int a = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-        int [][]arr= new int[a][a];
+        arr = new int[N][N];
+        dp = new Integer[N][N];
 
-//        for(int i = 0; i < a; i++)
-//        {
-//            st = new StringTokenizer(br.readLine());
-//            for(int k = 0; k < i+1; k++) {
-//                arr[i][k] = Integer.parseInt(st.nextToken());
-//            }
-//        }
-//
-//        for(int i = 0; i < a; i ++)
-//        {
-//            System.out.println();
-//            for(int k = 0; k< i +1; k ++)
-//            {
-//                System.out.print(arr[i][k]);
-//            }
-//        }
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int k = 0; k < i + 1; k++) {
+                arr[i][k] = Integer.parseInt(st.nextToken());
+            }
+        }
 
+        System.out.println(find(0, 0));
+    }
 
+    static int find(int depth, int idx) {
+        if (depth == N - 1) {
+            return arr[depth][idx];
+        }
 
-
+        if (dp[depth][idx] == null) {
+            dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+        }
+        return dp[depth][idx];
     }
 }
